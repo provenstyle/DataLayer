64324eb2a1306d16b359635c719989da32680e7b
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-09-21 12:38:11 -0500

    Adding Person and People Query

---
+++ Data/Queries/PeopleByFirstName.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Entities;
+using Highway.Data;
+
+namespace Data.Queries
+{
+    public class PeopleByFirstName : Query<Person>
+    {
+        public PeopleByFirstName(string firstName)
+        {
+            ContextQuery = context => context.AsQueryable<Person>()
+                .Where(x => x.FirstName == firstName);
+        }
+    }
+}
---
+++ Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
