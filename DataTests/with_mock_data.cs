6aaadfaa275cfe39a93f6ec39b0320ddfe2b383d
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-09-21 12:30:45 -0500

    Adding a mock data base class

---
+++ DataTests/with_mock_data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Highway.Data;
+using Machine.Specifications;
+using Moq;
+
+namespace DataTests
+{
+    public class with_mock_data
+    {
+        public static Mock<IDataContext> DataContext;
+        public static Repository Repository;
+
+        private Establish context = () =>
+            {
+                Repository = new Repository(DataContext.Object);
+            };
+
+        public static void MockDataContext<T>(List<T> data) where T : class 
+        {            
+            DataContext.Setup(x => x.AsQueryable<T>()).Returns(data.AsQueryable);
+        }
+        
+    }
+}
