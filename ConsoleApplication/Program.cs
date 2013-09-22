932868be950618cf5b531d1b139ba8d5188aefca
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-09-21 22:47:36 -0500

    Finishing the console app

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,16 +1,48 @@
 ﻿using System;
-using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Windsor;
+using Castle.Facilities.TypedFactory;
 using ProvenStyle.Data;
+using ProvenStyle.Entities;
 
 namespace ProvenStyle.ConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             var manager = new DatabaseManager("DataLayer");
             manager.DropCreateDatabase();
-            Console.WriteLine("Database exists: {0}", manager.ConnectionString);
+
+            var container = new WindsorContainer();
+            container.AddFacility<TypedFactoryFacility>();
+            container.Install(new DataInstaller());
+
+            var repositoryFactory = container.Resolve<IRepositoryFactory>();
+
+            repositoryFactory.WithRepository(r =>
+                                                 {
+                                                     r.Context.Add(new Person("Richard", "Castle"));
+                                                     r.Context.Add(new Person("Kate", "Becket"));
+                                                     r.Context.Add(new Person("Nikki", "Heat"));
+                                                     r.Context.Add(new Person("Derrick", "Storm"));
+                                                     r.Context.Add(new Person("Richard", "Nixon"));
+                                                     r.Context.Commit();
+                                                 });
+
+            var richards = new List<Person>();
+            repositoryFactory.WithRepository(r =>
+                                                 {
+                                                     richards = r.Find(new PeopleByFirstName("Richard")).ToList();
+                                                 });
+
+            Console.WriteLine("People named Richard");
+            richards.ForEach(r=>Console.WriteLine("{0} {1}", r.FirstName, r.LastName));
+            
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
