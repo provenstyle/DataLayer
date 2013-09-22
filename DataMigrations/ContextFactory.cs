0e83dddf7a50fbf6a015500b2bb1a422bee9f6b7
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-09-21 23:42:14 -0500

    Getting ready to enable migrations

---
+++ DataMigrations/ContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using Castle.Windsor;
+using Highway.Data;
+using ProvenStyle.Data;
+
+namespace DataMigrations
+{
+    public class ContextFactory : IDbContextFactory<DbContext>
+    {
+        public DbContext Create()
+        {
+            var container = new WindsorContainer();
+            container.Install(new DataInstaller());
+            var context = container.Resolve<IDataContext>();
+            return context as DbContext;
+        }
+    }
+}
